5a308315f58d2fc3a38cf562e2c49d06f83c98d1
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-02-17 16:42:57 +0600

    added discount facilities

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
+let itemCounter = 1;
+let totalPrice = 0;
+
 const cards = document.getElementsByClassName("card");
 
 for (const card of cards) {
   card.addEventListener("click", function () {
-    console.log(card);
     const title = card.querySelector("h3").innerText;
-    console.log(title);
+    const productPrice = parseFloat(card.querySelector("span").innerText.split(" ")[1]);
+    totalPrice += productPrice;
+    const titleContainer = document.getElementById("title-container");
+    const listItem = document.createElement("p");
+    listItem.innerText = `${itemCounter}. ${title}`;
+    itemCounter++;
+    titleContainer.appendChild(listItem);
+    const priceOnUi = document.getElementById("totalPrice");
+    priceOnUi.innerText = totalPrice.toFixed(2);
   });
 }
+
+document.getElementById("apply-btn").addEventListener("click", function () {
+  // Checking for discount
+  const discountValue = document
+    .getElementById("input-field")
+    .value.split(" ")
+    .join("")
+    .toUpperCase();
+  console.log(discountValue);
+  if (totalPrice >= 200) {
+    if (discountValue === "SELL200") {
+      const discountedPrice = totalPrice * 0.2;
+      const discountedPriceElement = document.getElementById("discountPrice");
+      discountedPriceElement.innerText = discountedPrice.toFixed(2);
+
+      // Remaining total
+      const total = document.getElementById("total");
+      const remainingTotal = totalPrice - discountedPrice;
+      total.innerText = remainingTotal.toFixed(2);
+    } else {
+      alert("Wrong coupon code");
+    }
+  } else {
+    alert("Spend more than 200$ to get the discount");
+  }
+  document.getElementById("input-field").value = "";
+});
